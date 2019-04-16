0ab759f00f543b170f6512432b023098abfb602d
jennifer-dutra <jrdutra@ucsc.edu>
2019-04-16 01:55:38 -0700

    navigation and routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+      <div>
+        <h2>Jennifer Dutra</h2> 
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul className="navbar-nav mr-auto">
+          <li><Link to={'/'} className="nav-link"> Portfolio </Link></li>
+          <li><Link to={'/about'} className="nav-link">About</Link></li>
+        </ul>
+        </nav>
+        <hr />
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
