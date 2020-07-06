4bd4107f057df5c6af35c060540ae38077b83f37
jrdutra <jrdutra@ucsc.edu>
2020-07-05 19:56:19 -0700

    Updates

--- precache-manifest.6fca4b6c2bd54415629188129112ef9e.js
+++ precache-manifest.6fca4b6c2bd54415629188129112ef9e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "0044a57a7df6e5a8d5847264bee4128e",
-    "url": "/static/media/linkedin.0044a57a.svg"
+    "revision": "f2985ff89f1eec853dc7b6438930e2d5",
+    "url": "/static/media/card-fronts-mock.f2985ff8.jpg"
   },
   {
-    "revision": "83a0a7a872551490167c",
-    "url": "/static/css/main.5b4973fa.chunk.css"
+    "revision": "26b0a03bab7e8592b2a1",
+    "url": "/static/css/main.2284f8a4.chunk.css"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "0a06c28e369e1d092605",
-    "url": "/static/js/2.93a012c1.chunk.js"
+    "revision": "7a162159086e56b6febf",
+    "url": "/static/js/2.3da19a25.chunk.js"
   },
   {
     "revision": "03a41fc62287f4ae605f3a63b06660d0",
@@ -32,8 +32,8 @@ self.__precacheManifest = [
     "url": "/static/media/heart.4a882bfa.svg"
   },
   {
-    "revision": "83a0a7a872551490167c",
-    "url": "/static/js/main.31f11e34.chunk.js"
+    "revision": "0044a57a7df6e5a8d5847264bee4128e",
+    "url": "/static/media/linkedin.0044a57a.svg"
   },
   {
     "revision": "431edc50406a80888f56de3e83cf0fb0",
@@ -48,12 +48,44 @@ self.__precacheManifest = [
     "url": "/static/media/menu.ca394ec8.svg"
   },
   {
+    "revision": "26b0a03bab7e8592b2a1",
+    "url": "/static/js/main.1adc0dd6.chunk.js"
+  },
+  {
+    "revision": "722787d08d3cef0d491ed7da3f4c6868",
+    "url": "/static/media/card-game-link.722787d0.jpg"
+  },
+  {
+    "revision": "f601e2211f8b94501e3477fe317949b2",
+    "url": "/static/media/event-mock.f601e221.jpg"
+  },
+  {
+    "revision": "aa3ec67f280dcac9e3d1f732391ac086",
+    "url": "/static/media/water-mock.aa3ec67f.jpg"
+  },
+  {
+    "revision": "724d55ab9cd698042d7687c8c4d8e9bd",
+    "url": "/static/media/packaging-mock.724d55ab.jpg"
+  },
+  {
+    "revision": "b9b78da08cdf614046dcb6b184aa8f13",
+    "url": "/static/media/rulebook.b9b78da0.jpg"
+  },
+  {
+    "revision": "34cb8951e166042d0a2109e7a02af6d6",
+    "url": "/static/media/visual-directions.34cb8951.png"
+  },
+  {
     "revision": "46b48004d21467f3b6de4519b3953f22",
     "url": "/static/media/boba-logo.46b48004.svg"
   },
   {
     "revision": "4ab10a4110d49ca68f30e6402471e811",
     "url": "/static/media/ch-logo.4ab10a41.svg"
+  },
+  {
+    "revision": "06068a16ab857edc99c171fbe95f6b42",
+    "url": "/static/media/microsoft-logo.06068a16.svg"
   },
   {
     "revision": "a9b89f679304d91eb65a934db095baf5",
@@ -64,7 +96,7 @@ self.__precacheManifest = [
     "url": "/static/media/wilderness-logo.6d67e185.svg"
   },
   {
-    "revision": "5e5eed370ba7476146cd3d214ee46961",
+    "revision": "f067f953c8505c4f71cf6fa840ae35d5",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3272751aef296139eaee3467c3d69c12.js"
+  "/precache-manifest.6fca4b6c2bd54415629188129112ef9e.js"
 );
 
 workbox.clientsClaim();
